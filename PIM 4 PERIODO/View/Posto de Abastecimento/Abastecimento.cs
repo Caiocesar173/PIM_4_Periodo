54cfa86dc69c14999be51ca94c01f25f5a4f71f4
Eduardo <Eduardo@DESKTOP-2699SR0>
2019-11-15 23:31:23 -0300

    Tela de Abastecimento, concluido.

---
+++ PIM 4 PERIODO/View/Posto de Abastecimento/Abastecimento.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PIM_4_PERIODO.View.Posto_de_Abastecimento
+{
+    public partial class Abastecimento : Form
+    {
+        public Abastecimento()
+        {
+            InitializeComponent();
+        }
+    }
+}
